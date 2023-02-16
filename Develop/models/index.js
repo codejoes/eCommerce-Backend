452627775a23ceaf0a0cff06ad1503bf5557a76d
codejoes <josephscodes@gmail.com>
2023-02-15 22:35:52 -0500

    fix model index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,34 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "category_name",
+  foreignKey: "category_id",
   onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "category_name",
+  foreignKey: "category_id",
   onDelete: "CASCADE",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: "product_tags"
+  through: ProductTag,
+  foreignKey: 'product_id'
 })
 
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-
-  as: "tag_productTags"
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
